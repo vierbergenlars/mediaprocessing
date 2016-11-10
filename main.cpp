2692f4a2963aa3ec948027ca70d38dc015245e9d
Lars Vierbergen <vierbergenlars@gmail.com>
2016-11-10 12:33:09 +0100

    Read filename of map from arguments and handle non-exiting file error

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 #include <world.h>
+
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    if(argc < 2) {
+        std::cerr << "Pass the world file to use as first parameter" << std::endl;
+        return 1;
+    }
+    World *w = new World();
+    try {
+        auto tiles = w->createWorld(argv[1]);
+    } catch(const QString &err) {
+        std::cerr << err.toStdString() << std::endl;
+        return 2;
+    }
+
 }
