29c619c2f54e7f077a4b684364141a550487d3ff
Lars Vierbergen <vierbergenlars@gmail.com>
2016-11-18 18:15:31 +0100

    Add more optimized Matrix

--- matrix.h
+++ matrix.h
@@ -2,46 +2,49 @@
 #define MATRIX_H
 #include <vector>
 #include <iostream>
+#include <iterator>
+#include <cassert>
 
-template<class T>
-class Matrix;
-
-template<class T>
-std::ostream& operator<<(std::ostream &stream, const Matrix<T> &matrix);
 
 template<class T>
 class Matrix
 {
 public:
     Matrix(int rows, int cols);
-    Matrix<T> slice(int xStart, int yStart, int xEnd, int yEnd) const;
+    Matrix<T> slice(int xStart, int yStart, int xEnd, int yEnd);
     std::vector<T> all() const;
 
+    void setRef(int x, int y, T& object);
     void set(int x, int y, T object);
-    T &get(int x, int y) const;
+    T &get(int x, int y);
+    const T &get(int x, int y) const;
     int rows() const { return _rows; }
     int cols() const { return _cols; }
-    friend std::ostream& operator<<<>(std::ostream &stream, const Matrix<T> &matrix);
 private:
+    inline void __verify_offsets(int x, int y) const;
     int _rows;
     int _cols;
-    std::vector<std::vector<T>> storage;
+    std::vector<T> storage;
 };
 
 
 template<class T>
-Matrix<T>::Matrix(int rows, int cols):
-    _rows(rows), _cols(cols), storage(rows, std::vector<T>(cols))
+Matrix<T>::Matrix(int xLen, int yLen):
+    _rows(xLen), _cols(yLen), storage(xLen*yLen)
 {
 }
 
 template<class T>
-Matrix<T> Matrix<T>::slice(int xStart, int yStart, int xEnd, int yEnd) const
+Matrix<T> Matrix<T>::slice(int xStart, int yStart, int xEnd, int yEnd)
 {
+    __verify_offsets(xStart, yStart);
+    __verify_offsets(xEnd, yEnd);
+    assert(xStart < xEnd);
+    assert(yStart < yEnd);
     Matrix<T> matrix(xEnd-xStart, yEnd-yStart);
     for(int i=xStart; i < xEnd; i++) {
         for(int j=yStart; j < yEnd; j++) {
-            matrix.set(i-xStart, j-yStart, storage[i][j]);
+            matrix.setRef(i-xStart, j-yStart, this->get(i, j));
         }
     }
     return matrix;
@@ -51,36 +54,57 @@ Matrix<T> Matrix<T>::slice(int xStart, int yStart, int xEnd, int yEnd) const
 template<class T>
 std::vector<T> Matrix<T>::all() const
 {
-    std::vector<T> result(_rows*_cols);
-    for(std::vector<T> &i: storage) {
-        for(T &j: i) {
-            result.push_back(j);
-        }
-    }
-    return result;
+    return storage;
 }
 
 template<class T>
-T& Matrix<T>::get(int x, int y) const
+T& Matrix<T>::get(int x, int y)
 {
-    return storage[x][y];
+    __verify_offsets(x, y);
+    return storage[x*_cols+y];
+}
+
+template<class T>
+const T& Matrix<T>::get(int x, int y) const
+{
+    __verify_offsets(x, y);
+    return storage[x*_cols+y];
 }
 
 template<class T>
 void Matrix<T>::set(int x, int y, T object)
 {
-    storage[x][y] = object;
+    __verify_offsets(x, y);
+    storage[x*_cols+y] = object;
+}
+
+template<class T>
+void Matrix<T>::setRef(int x, int y, T& object)
+{
+    __verify_offsets(x, y);
+    storage[x*_cols+y] = object;
+}
+
+template<class T>
+inline void Matrix<T>::__verify_offsets(int x, int y) const
+{
+    assert(x >= 0);
+    assert(y >= 0);
+    assert(x < this->rows());
+    assert(y < this->cols());
 }
 
 template<class T>
 std::ostream& operator<<(std::ostream &stream, const Matrix<T> &matrix)
 {
-    for(auto &row: matrix.storage) {
-        for(auto &cell: row) {
-            stream << cell << " ";
+    for(int i=0; i < matrix.rows(); i++) {
+        for(int j=0; j < matrix.cols(); j++) {
+            stream << matrix.get(i, j);
+            if(j < matrix.cols() - 1)
+                stream << " ";
         }
-        stream << std::endl;
-
+        if (i < matrix.rows() - 1)
+            stream << std::endl;
     }
     return stream;
 }
