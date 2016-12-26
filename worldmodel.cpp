f75d56e5654a1c3b5f5b34ee7bf14d0f456d2a5c
Lars Vierbergen <vierbergenlars@gmail.com>
2016-12-26 12:44:05 +0100

    Make generic method to get tiles around a certain tile

--- worldmodel.cpp
+++ worldmodel.cpp
@@ -9,11 +9,16 @@ WorldModel::WorldModel(Matrix<std::shared_ptr<WorldTile> > *tiles, std::shared_p
 
 std::unique_ptr<Matrix<std::shared_ptr<WorldTile> > > WorldModel::tilesAroundProtagonist(int range)
 {
-    int rowStart = std::max(0, _protagonist->getYPos() - range);
-    int colStart = std::max(0, _protagonist->getXPos() - range);
+    return tilesAround(_protagonist, range);
+}
 
-    int rowEnd = std::min(_tiles->rows()-1, _protagonist->getYPos() + range);
-    int colEnd = std::max(_tiles->cols()-1, _protagonist->getXPos() + range);
+std::unique_ptr<Matrix<std::shared_ptr<WorldTile> > > WorldModel::tilesAround(std::shared_ptr<Tile> tile, int range)
+{
+    int rowStart = std::max(0, tile->getYPos() - range);
+    int colStart = std::max(0, tile->getXPos() - range);
+
+    int rowEnd = std::min(_tiles->rows()-1, tile->getYPos() + range);
+    int colEnd = std::min(_tiles->cols()-1, tile->getXPos() + range);
 
     return _tiles->slice(rowStart, colStart, rowEnd, colEnd);
 }
